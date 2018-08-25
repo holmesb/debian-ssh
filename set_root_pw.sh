25e495056a122151cd7ba05afb1a06ac4490f924
holmesb <me@brendanh.com>
2018-08-25 18:45:46 +0100

    Update set_root_pw.sh

--- set_root_pw.sh
+++ set_root_pw.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 if [ -z "${SSH_KEY}" ]; then
-	echo "=> Please pass your public key in the SSH_KEY environment variable"
+	echo "=> Please pass your key in the SSH_KEY environment variable"
 	exit 1
 fi
 
 for MYHOME in /root /home/docker; do
 	echo "=> Adding SSH key to ${MYHOME}"
 	mkdir -p ${MYHOME}/.ssh
-	chmod go-rwx ${MYHOME}/.ssh
-	echo "${SSH_KEY}" > ${MYHOME}/.ssh/authorized_keys
-	chmod go-rw ${MYHOME}/.ssh/authorized_keys
+	chmod 700 ${MYHOME}/.ssh
+	echo "${SSH_KEY}" > ${MYHOME}/.ssh/id_rsa
+	chmod 600 ${MYHOME}/.ssh/id_rsa
 	echo "=> Done!"
 done
 chown -R docker:docker /home/docker/.ssh
-
-echo "========================================================================"
-echo "You can now connect to this container via SSH using:"
-echo ""
-echo "    ssh -p <port> <user>@<host>"
-echo ""
-echo "Choose root (full access) or docker (limited user account) as <user>."
-echo "========================================================================"
